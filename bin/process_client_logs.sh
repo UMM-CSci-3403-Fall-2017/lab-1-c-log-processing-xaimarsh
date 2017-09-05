10b194296246cce2955c044768152a3fb9606f7d
Xaitheng Yang <yang3792@mylar.morris.umn.edu>
2017-09-05 18:19:52 -0500

    Our code is working imperfectly. Our output appears to be missing some results when compared to the expected output for process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 dir=$1
 
-cd $dir/var/log
+cd $dir
 
+touch failed_login_data.txt
 
-
+for f in ./var/log/*
+do
+	awk '/Failed password for invalid user (.[A-Z|a-z]*)\s/ {print $1,$2,substr($3,0,2),$11,$13} /Failed password for ([a-zA-z]*) from/ {print $1,$2,substr($3,0,2),$9,$11}' $f >> failed_login_data.txt
+done
 
