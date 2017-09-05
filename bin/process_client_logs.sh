61fb472b36a4b497ce4de6120567b38ab08dd629
Xaitheng Yang <yang3792@mylar.morris.umn.edu>
2017-09-05 18:32:17 -0500

    Completed, to the satisfaction of the bats test, process_client_logs.sh. Praise the sun

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -7,6 +7,6 @@ touch failed_login_data.txt
 
 for f in ./var/log/*
 do
-	awk '/Failed password for invalid user (.[A-Z|a-z]*)\s/ {print $1,$2,substr($3,0,2),$11,$13} /Failed password for ([a-zA-z]*) from/ {print $1,$2,substr($3,0,2),$9,$11}' $f >> failed_login_data.txt
+	awk '/Failed password for invalid user ([a-zA-Z0-9]*)\s/ {print $1,$2,substr($3,0,2),$11,$13} /Failed password for ([a-zA-Z0-9]*) from/ {print $1,$2,substr($3,0,2),$9,$11}' $f >> failed_login_data.txt
 done
 
