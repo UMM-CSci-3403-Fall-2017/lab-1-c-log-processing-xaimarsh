b36f09f97b56dae12cbce6a34b03777f3369ddfb
Xaitheng Yang <yang3792@macross.morris.umn.edu>
2017-09-07 11:48:16 -0500

    Add comments Remame temporary files from 'names' to 'hours' to more accurately represent what this script is doing

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,22 +1,27 @@
+##Gets first argument passed from the command line
 dir=$1
 
+##Loop to find all files in subdirectories of the $dir directory that contains failed_login_data.txt and prints the hour of the of the day in the created file tempHours.txt
 for f in $dir/*
 do
-        awk '{print $3}' $f/failed_login_data.txt >> tempNames.txt
+        awk '{print $3}' $f/failed_login_data.txt >> tempHours.txt
 done
 
-sort tempNames.txt >> sortedTempNames.txt
+##Sorts the tempHours.txt by hours of the day (ex. 1 before 9)
+sort tempHours.txt >> sortedTempHours.txt
 
-uniq -c sortedTempNames.txt >> uniqNames.txt
+##Gets rid of any repeat data
+uniq -c sortedTempHours.txt >> uniqHours.txt
 
-awk '{print "\t\tdata.addRow([\x27"$2"\x27, "$1"]);"}' uniqNames.txt  >> toBeGiven.txt
+##Formats data to the liking of html
+awk '{print "\t\tdata.addRow([\x27"$2"\x27, "$1"]);"}' uniqHours.txt  >> toBeGiven.txt
 
+##Runs our wrap_contents.sh script, and creates a file at $dir/hours_dist.html
 bin/wrap_contents.sh toBeGiven.txt  html_components/hours_dist $dir/hours_dist.html
 
-##mv username_dist.html $dir
-
-rm tempNames.txt
-rm sortedTempNames.txt
-rm uniqNames.txt
+##Removes temporary files
+rm tempHours.txt
+rm sortedTempHours.txt
+rm uniqHours.txt
 rm toBeGiven.txt
 
