165f5e64c7375a0faf53064e4bbe8c8f9f0abb73
Xaitheng Yang <yang3792@macross.morris.umn.edu>
2017-09-07 11:54:05 -0500

    add comments and alter create_username_dist.sh script to be more readable.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,20 +1,25 @@
+##gets 1st command line argument
 dir=$1
 
+##for loop to retrieve data from failed_login_data.txt in each subdirectory of $dir
 for f in $dir/*
 do
 	awk '{print $4}' $f/failed_login_data.txt >> tempNames.txt
 done
 
+## sorts the data in tempNames.txt
 sort tempNames.txt >> sortedTempNames.txt
 
+## tabulates instances of each name from sortedTempNames
 uniq -c sortedTempNames.txt >> uniqNames.txt
 
+## formats the result of uniqNames to be used in html
 awk '{print "\t\tdata.addRow([\x27"$2"\x27, "$1"]);"}' uniqNames.txt  >> toBeGiven.txt
 
+##wraps the contents of toBeGiven.txt with an html formatted header and footer
 bin/wrap_contents.sh toBeGiven.txt  html_components/username_dist $dir/username_dist.html
 
-##mv username_dist.html $dir
-
+##removes temporary files
 rm tempNames.txt
 rm sortedTempNames.txt
 rm uniqNames.txt
