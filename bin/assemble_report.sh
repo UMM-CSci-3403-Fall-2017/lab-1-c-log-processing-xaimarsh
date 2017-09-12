6a05e4d1dd88ec31f36212c50316a9535e585b71
Xaitheng Yang <yang3792@mylar.morris.umn.edu>
2017-09-12 11:32:43 -0500

    Script appears to work when tested on scratch directory, but fails bats test

---
+++ bin/assemble_report.sh
@@ -0,0 +1,9 @@
+dir=$1
+
+##Combines our three html files to one file
+cat $dir/username_dist.html $dir/hours_dist.html $dir/country_dist.html >> endGame.html
+
+##Uses our wrap_contents.sh script to create a usable html file with all three graphs
+bin/wrap_contents.sh endGame.html html_components/summary_plots $dir/failed_login_summary.html
+
+rm endGame.html
