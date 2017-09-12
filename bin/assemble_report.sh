c9a7e70c0da150dca540a0690793285fc33848a5
Xaitheng Yang <yang3792@mylar.morris.umn.edu>
2017-09-12 11:42:52 -0500

    Complete assemble_report.sh

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,9 +1,10 @@
 dir=$1
 
 ##Combines our three html files to one file
-cat $dir/username_dist.html $dir/hours_dist.html $dir/country_dist.html >> endGame.html
+cat $dir/country_dist.html $dir/hours_dist.html $dir/username_dist.html >> endGame.html
 
 ##Uses our wrap_contents.sh script to create a usable html file with all three graphs
 bin/wrap_contents.sh endGame.html html_components/summary_plots $dir/failed_login_summary.html
 
+##Remove temp file
 rm endGame.html
