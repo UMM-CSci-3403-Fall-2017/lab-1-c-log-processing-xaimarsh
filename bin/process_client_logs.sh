8b35343746e697a5978d9bb7f3d70c13830a3fad
Xaitheng Yang <yang3792@acrylic.morris.umn.edu>
2017-09-19 12:12:12 -0500

    Fixed issue with regex expression that leaves out fringe cases (cases that included punctuation in username)

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,6 +3,10 @@ dir=$1
 
 for f in $dir/var/log/*
 do
-	awk '/Failed password for invalid user ([a-zA-Z0-9]*)\s/ {print $1,$2,substr($3,0,2),$11,$13} /Failed password for ([a-zA-Z0-9]*) from/ {print $1,$2,substr($3,0,2),$9,$11}' $f >> $dir/failed_login_data.txt
+	awk '/Failed password for invalid user ([a-zA-Z0-9[:punct:]]*) from/ {print $1,$2,substr($3,0,2),$11,$13} /Failed password for ([a-zA-Z0-9[:punct:]]*) from/ {print $1,$2,substr($3,0,2),$9,$11}' $f >> $dir/failed_login_data.txt
 done
+
+#do
+#        awk '/Failed password for invalid user ([a-zA-Z0-9]*)\s/ {print $1,$2,substr($3,0,2),$11,$13} /Failed password for ([a-zA-Z0-9]*) from/ {print $1,$2,substr($3,0,2),$9,$11}' $f >> $dir/failed_login_data.txt
+#done
 
